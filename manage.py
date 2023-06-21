5c75cdca9ba8f99dffcb3e5350d4be6e7bb342f4
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-06-22 00:01:05 +0100

    updated settings

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ def main():
         load_dotenv('.env')
 
     # When running on Azure App Service you should use the production settings.
-    settings_module = "capaggregator.production" if 'WEBSITE_HOSTNAME' in os.environ else 'capaggregator.settings'
+    settings_module = "capaggregator.production"
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
     try:
