dc4198802d61612db90a67a60a82525639774219
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-06-22 15:57:22 +0100

    update settings/production

--- capaggregator/production.py
+++ capaggregator/production.py
@@ -42,5 +42,3 @@ DATABASES = {
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_CACHE_BACKEND = 'django-cache'
