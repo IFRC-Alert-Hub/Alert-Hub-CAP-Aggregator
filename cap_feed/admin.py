c931f8cfd8344706dc45b8e1887fda97a41b2a8d
Alfiejz <alfiejz@outlook.com>
2023-06-22 16:43:41 +0100

    added region data

--- cap_feed/admin.py
+++ cap_feed/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Alert
+from .models import Alert, Region, Country
 
 # Register your models here.
 
 admin.site.register(Alert)
+admin.site.register(Region)
+admin.site.register(Country)
