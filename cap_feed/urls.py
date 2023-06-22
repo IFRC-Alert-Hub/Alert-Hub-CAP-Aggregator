9c08f5839de0f0d48f273354da23793af2ba8477
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-06-22 16:52:32 +0100

    update settings

--- cap_feed/urls.py
+++ cap_feed/urls.py
@@ -4,5 +4,6 @@ from . import views
 from django.contrib import admin
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', admin.site.urls, 'admin'),
     path('pollingalerts/', views.polling_alerts, name = 'polling_alerts')
 ]
