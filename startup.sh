84f026fb3ccb734ac4d5b034f45f10395f4f6e20
Alfiejz <alfiejz@outlook.com>
2023-06-23 17:28:37 +0100

    changed startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 python manage.py migrate
 gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
-     --chdir=/home/site/wwwroot capaggregator.wsgi
-celery -A capaggregator worker -l info --pool=solo
+     --chdir=/home/site/wwwroot capaggregator.wsgi & celery -A capaggregator worker -l info --pool=solo & celery -A capaggregator beat -l info
