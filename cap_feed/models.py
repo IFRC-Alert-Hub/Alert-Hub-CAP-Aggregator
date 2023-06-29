5dfc3af739a31d3f4a01005c9c6b42ee049a36de
Alfiejz <alfiejz@outlook.com>
2023-06-29 11:42:50 +0100

    fixed merge duplication

--- cap_feed/models.py
+++ cap_feed/models.py
@@ -36,28 +36,6 @@ class Country(models.Model):
     def __str__(self):
         return self.name
     
-
-class Region(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
-    name = models.CharField(max_length=255)
-    polygon = models.TextField(max_length=16383, blank=True, default='')
-    centroid = models.CharField(max_length=255, blank=True, default='')
-
-    def __str__(self):
-        return self.name
-
-class Country(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
-    name = models.CharField(max_length=255)
-    iso = models.CharField(max_length=255, blank=True, default='')
-    iso3 = models.CharField(max_length=255, blank=True, default='')
-    polygon = models.TextField(max_length=16383, blank=True, default='')
-    centroid = models.CharField(max_length=255, blank=True, default='')
-    region = models.ForeignKey(Region, on_delete=models.SET_DEFAULT, default='-1')
-
-    def __str__(self):
-        return self.name
-    
 class Alert(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     identifier = models.CharField(max_length=255)
