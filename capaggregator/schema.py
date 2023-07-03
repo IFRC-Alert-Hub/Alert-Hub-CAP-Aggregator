4ec7f02fee4695e911f4b4bec62b5872ef96594e
Alfiejz <alfiejz@outlook.com>
2023-07-03 19:12:36 +0100

    added graphql filters for alerts and countries

--- capaggregator/schema.py
+++ capaggregator/schema.py
@@ -1,45 +1,60 @@
 import graphene
-from graphene_django import DjangoObjectType #used to change Django object into a format that is readable by GraphQL
-from django.utils import timezone
-from cap_feed.models import Alert, Continent, Region, Country
+from graphene_django import DjangoObjectType
+from cap_feed.models import Continent, Region, Country, Alert
+
+
 
 class AlertType(DjangoObjectType):
     class Meta:
         model = Alert
-        field = ("id", "identifier", "sender", "sent", "status", "msg_type", "scope", "urgency", "severity", "certainty", "effective", "expires", "area_desc", "event", "geocode_name", "geocode_value")
 
 class ContinentType(DjangoObjectType):
     class Meta:
         model = Continent
-        field = ("id", "name")
 
 class RegionType(DjangoObjectType):
     class Meta:
         model = Region
-        field = ("id", "name", "polygon", "centroid")
 
 class CountryType(DjangoObjectType):
     class Meta:
         model = Country
-        field = ("id", "name", "iso3", "polygon", "multipolygon")
 
 
 class Query(graphene.ObjectType):
-    list_alert=graphene.List(AlertType)
-    list_continents=graphene.List(ContinentType)
-    list_country=graphene.List(CountryType)
+    list_alert=graphene.List(AlertType, iso3=graphene.String(), region_id=graphene.String(), continent_id=graphene.String())
+    list_continent=graphene.List(ContinentType)
+    list_country=graphene.List(CountryType, region_id=graphene.String(), continent_id=graphene.String())
     list_region=graphene.List(RegionType)
 
-    def resolve_list_alert(root, info):
-        return Alert.objects.order_by("-id")
+    def resolve_list_alert(root, info, iso3=None, region_id=None, continent_id=None, **kwargs):
+        filter = dict()
+        if iso3:
+            filter['country__iso3'] = iso3
+        if region_id:
+            filter['country__region'] = region_id
+        if continent_id:
+            filter['country__continent'] = continent_id
+        if len(filter) > 0:
+            return Alert.objects.filter(**filter).all()
+
+        return Alert.objects.all()
     
     def resolve_list_continent(root, info):
-        return Continent.objects.order_by("-id")
+        return Continent.objects.all()
     
-    def resolve_list_country(root, info):
-        return Country.objects.order_by("-id")
+    def resolve_list_country(root, info, region_id=None, continent_id=None, **kwargs):
+        filter = dict()
+        if region_id:
+            filter['region__id'] = region_id
+        if continent_id:
+            filter['continent__id'] = continent_id
+        if len(filter) > 0:
+            return Country.objects.filter(**filter).all()
+        
+        return Country.objects.all()
     
     def resolve_list_region(root, info):
-        return Region.objects.order_by("-id")
+        return Region.objects.all()
 
 schema = graphene.Schema(query=Query)
