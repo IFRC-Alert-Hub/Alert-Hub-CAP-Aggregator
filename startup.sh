00131a3315f2c4c5ba487d1a8ccbc17140bfc5c7
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-05 22:28:32 +0100

    Update daphne settings

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 python manage.py migrate
-daphne -b 0.0.0.0 -p 8000 capaggregator.asgi:application & python manage.py runworker & celery -A capaggregator worker -l info --pool=solo & celery -A capaggregator beat -l info
+gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
+    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
+     --chdir=/home/site/wwwroot capaggregator.wsgi & daphne -b 0.0.0.0 -p 8001 capaggregator.asgi:channel_layer & celery -A capaggregator worker -l info --pool=solo & celery -A capaggregator beat -l info
