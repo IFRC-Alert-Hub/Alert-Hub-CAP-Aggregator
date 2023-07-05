cb65a942c5b1e9d49a035816470f5651096f7e9b
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-05 18:12:54 +0100

    Fix integration bugs

--- alert_subscription/models.py
+++ alert_subscription/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Record the currently connected channels
-'''
-class Channel(models.Model):
-    channel_name = models.CharField(max_length=255,default="", primary_key=True)
-'''
