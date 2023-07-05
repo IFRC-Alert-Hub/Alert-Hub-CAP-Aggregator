3b6666daea60b04d4b5ca3f8867c7fcae3a7a501
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-05 01:27:00 +0100

    Update settings for deployment

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 python manage.py migrate
-daphne capaggregator.asgi:application & celery -A capaggregator worker -l info --pool=solo & celery -A capaggregator beat -l info
+daphne -u capaggregator.asgi:application -p 8000 & celery -A capaggregator worker -l info --pool=solo & celery -A capaggregator beat -l info
