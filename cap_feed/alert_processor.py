360bd3001f97e12e43c77754eef9e5e75657b634
Alfiejz <alfiejz@outlook.com>
2023-07-11 02:18:56 +0100

    fixed broken tests from alert model change

--- cap_feed/alert_processor.py
+++ cap_feed/alert_processor.py
@@ -1,4 +1,4 @@
-from .models import Alert, Country, Source
+from .models import Alert, AlertInfo, Source
 from django.utils import timezone
 import cap_feed.formats.format_handler as fh
 
@@ -15,4 +15,5 @@ def poll_new_alerts():
         fh.get_alerts(format, url, country, ns)
 
 def remove_expired_alerts():
-    Alert.objects.filter(expires__lt=timezone.now()).delete()
+    AlertInfo.objects.filter(expires__lt=timezone.now()).delete()
+    Alert.objects.filter(alertinfo__isnull=True).delete()
