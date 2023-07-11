94f903a44bcb599f0e9a8c7249c1c1654ecfd0a6
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-11 14:14:38 +0100

    Debug for websocket connection

--- capaggregator/settings.py
+++ capaggregator/settings.py
@@ -176,3 +176,17 @@ CHANNEL_LAYERS = {
         }
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
