f96b3c4c7764f33a9e26f3c48ea9acccf03270a9
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-11 10:55:50 +0100

    Setup allowed origin for websocket connection

--- capaggregator/asgi.py
+++ capaggregator/asgi.py
@@ -37,7 +37,7 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": OriginValidator(
         AuthMiddlewareStack(URLRouter(capaggregator.routing.websocket_urlpatterns)),
-        ["wss://localhost"]
+        ["wss://localhost", "wss://https://backend-develop.scm.azurewebsites.net/"]
     ),
 })
 
