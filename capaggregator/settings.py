27c11819295ca77d849c5d20e7f12d3089ce9eae
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-17 23:57:45 +0100

    Update Django Cache to Redis

--- capaggregator/settings.py
+++ capaggregator/settings.py
@@ -177,3 +177,12 @@ CHANNEL_LAYERS = {
     },
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/2',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
