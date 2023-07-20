a84395b0bbd715b582d75b17961b7ef4e5133d19
Alfiejz <alfiejz@outlook.com>
2023-07-20 18:14:07 +0100

    changed whitenoise staticfiles storage setting

--- capaggregator/production.py
+++ capaggregator/production.py
@@ -22,8 +22,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # WHITENOISE_USE_FINDERS = True
 # WHITENOISE_MANIFEST_STRICT = False
 # WHITENOISE_ALLOW_ALL_ORIGINS = True
