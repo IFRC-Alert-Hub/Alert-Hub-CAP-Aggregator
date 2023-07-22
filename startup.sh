0188480d3bc9c48a0fe8a689af224dc3cfcc549a
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-22 23:53:24 +0100

    Try fixing deployment bugs on Azure

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 python manage.py migrate
+python manage.py collectstatic
 daphne -b 0.0.0.0 -p 8000 capaggregator.asgi:application & celery -A capaggregator worker -l info --pool=solo & celery -A capaggregator beat -l info
