08df3be173b952ab6f65b0faf6a0369ae8daf0a8
Alfiejz <alfiejz@outlook.com>
2023-07-31 19:47:29 +0100

    Enabled concurrent Celery workers for Azure

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
-daphne -b 0.0.0.0 -p 8000 capaggregator.asgi:application & celery -A capaggregator worker -l info --pool=solo & celery -A capaggregator beat -l info
+daphne -b 0.0.0.0 -p 8000 capaggregator.asgi:application & celery -A capaggregator worker -l info -c 4 & celery -A capaggregator beat -l info
